80bc0c94de3215b7fb36e27d4f7f3c3bb1a71c40
Tobias Grassani <tobigrassani@gmail.com>
2022-09-21 23:34:40 -0300

    Update Data Types Handling.py

---
+++ Data Types Handling.py
@@ -0,0 +1,19 @@
+""" Python Data Types:
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+x = str("Hello World!")
+print("This variable is a:", type(x), "and its value is:", x)
+x = int(5)
+print("This variable is a:", type(x), "and its value is:", x)
+x = float(2.5)
+print("This variable is a:", type(x), "and its value is:", x)
+x = complex(3j)
+print("This variable is a:", type(x), "and its value is:", x)
+x = list
