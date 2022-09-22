e05ed280e8ba8697448960d9a8867dfe2c40e49a
Tobias Grassani <tobigrassani@gmail.com>
2022-09-21 21:58:09 -0300

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,5 +1,10 @@
+# A variable its created when a value is assigned.
+# Create & assign: (name) = (value)
 x = 5
 y = 2
-
+z = "Josh"
 if x > y:
     print(x, "is greater than", y)
+    print(z, "born in New York.")
+
+
