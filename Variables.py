6a0522b411ccc33575676bd42a84ea5722f6437f
Tobias Grassani <tobigrassani@gmail.com>
2022-09-21 22:07:22 -0300

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -7,4 +7,11 @@ if x > y:
     print(x, "is greater than", y)
     print(z, "born in New York.")
 
+# Casting a variable means to assign a value & a data type once the variable its created
+x = str(5)
+y = int(5)
+z = float(3.14)
+print("x is string:",x)
+print("y is integrer:",y)
+print("z is float:",z,"and adding",y,"=",z+y)
 
