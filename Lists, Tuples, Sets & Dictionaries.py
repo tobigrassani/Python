dbd5cc2072dd8af7278e8435e1b7e829adf52b65
Tobias Grassani <tobigrassani@gmail.com>
2022-09-24 15:25:24 -0300

    Update Lists, Tuples, Sets & Dictionaries.py

--- Lists, Tuples, Sets & Dictionaries.py
+++ Lists, Tuples, Sets & Dictionaries.py
@@ -6,7 +6,13 @@ Set is a collection which is unordered, unchangeable*, and unindexed. No duplica
 Dictionary is a collection which is ordered** and changeable. No duplicate members.
 """
 
-# LISTS
+# LISTS [ , , ]
+
+thislist = [1, 2, 3, 4]
+print(thislist.__len__())             # 4
+print(thislist.__class__)             # <class 'list'>
+print(thislist[0] + thislist[1])      # 3
+print(thislist[1:3])                  # [2, 3] ( index 1 included, index 3 not includes )
 
 
 
