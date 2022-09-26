61a72c7a797ea89737c6bf96851307faf78042f3
Tobias Grassani <tobigrassani@gmail.com>
2022-09-26 20:51:53 -0300

    Update

--- Functions & Loops.py
+++ Functions & Loops.py
@@ -12,6 +12,27 @@ if  else
 while
 for
 """
+#   EXAMPLES
 a = 2
 b = 330
 print("A") if a > b else print("B")     # reducing lines
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+a = 200
+b = 33
+c = 500
+if (a > b) and (c > a):
+    print("Both conditions are True")
+
+#   NESTED IF
+x = 17
+
+if x > 10:
+    print("Above ten,")
+if x > 20:
+    print("and also above 20!")
+else:
+    print("but not above 20.")
