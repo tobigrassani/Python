e9a6e6265c9acf98b6dcd12cf622770c51d9fa9a
Tobias Grassani <tobigrassani@gmail.com>
2022-09-26 20:46:36 -0300

    Update new file

---
+++ Functions & Loops.py
@@ -0,0 +1,17 @@
+#Conditions and Statements
+"""
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+
+if  else
+    elif  "if the previous conditions were not true, then try this condition"
+while
+for
+"""
+a = 2
+b = 330
+print("A") if a > b else print("B")     # reducing lines
