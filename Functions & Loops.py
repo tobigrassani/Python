23cee62fb6c7d3c5ce37909c0012ec7355ddeebb
Tobias Grassani <tobigrassani@gmail.com>
2022-09-27 20:20:38 -0300

    Update Functions & Loops.py

--- Functions & Loops.py
+++ Functions & Loops.py
@@ -38,7 +38,7 @@ else:
     print("but not above 20.")
 
 
-    #duplicadoooo
+    #duplicadoooo por dos
 
 
 if x > 10:
@@ -47,12 +47,4 @@ if x > 20:
     print("and also above 20!")
 else:
     print("but not above 20.")
-
-    if x > 10:
-        print("Above ten,")
-    if x > 20:
-        print("and also above 20!")
-    else:
-        print("but not above 20.")
-
 
