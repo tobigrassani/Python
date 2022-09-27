22a8ad8422db9b53e74366057d31c50cb0c310c2
Tobias Grassani <tobigrassani@gmail.com>
2022-09-27 20:20:26 -0300

    Update Functions & Loops.py

--- Functions & Loops.py
+++ Functions & Loops.py
@@ -47,3 +47,12 @@ if x > 20:
     print("and also above 20!")
 else:
     print("but not above 20.")
+
+    if x > 10:
+        print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+
+
