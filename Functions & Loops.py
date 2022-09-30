93024841bef20a89327499985434b5daffeb2991
Tobias Grassani <tobigrassani@gmail.com>
2022-09-29 21:52:30 -0300

    UPDATE

--- Functions & Loops.py
+++ Functions & Loops.py
@@ -18,8 +18,21 @@ b = 330
 print("A") if a > b else print("B")     # reducing lines
 
 a = 330
-b = 330
+b = 3301
 print("A") if a > b else print("=") if a == b else print("B")
+"""
+STATEMENT 1:
+print("A") if a > b else print("=") if a == b else print("B")
+Its equal to
+STATEMENT 2:
+if a > b
+    print("A")
+else
+    if a==b
+        print("=")
+    else
+        print("B")
+"""
 
 a = 200
 b = 33
@@ -36,6 +49,9 @@ if x > 20:
     print("and also above 20!")
 else:
     print("but not above 20.")
+
+
+
 
 
 
