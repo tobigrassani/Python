060a14b021eff6bcc15e72a23dae43b3cf7ef070
Tobias Grassani <tobigrassani@gmail.com>
2022-09-29 22:59:52 -0300

    update

--- Functions & Loops.py
+++ Functions & Loops.py
@@ -158,6 +158,19 @@ print(multiplicate(4,2))
 print(multiplicate(1,1))
 print(multiplicate())
 
+# recursion
+def tri_recursion(k):
+    if k > 0:
+        result = k + tri_recursion(k - 1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
 
 
 
