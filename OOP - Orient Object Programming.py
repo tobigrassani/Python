fd39c8f44a634612f7bb5a2222a1186b3ec71163
Tobias Grassani <tobigrassani@gmail.com>
2022-10-01 00:21:18 -0300

    Update OOP - Orient Object Programming.py

--- OOP - Orient Object Programming.py
+++ OOP - Orient Object Programming.py
@@ -58,3 +58,38 @@ class Puppy(Dog):
 
 x = Puppy("child1", 0.5)
 x.intro()
+
+
+class MyIter:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+theClass = MyIter()
+theIter = iter(theClass)
+
+for x in range(10):
+    print(next(theIter))
+
+
+class MyIter:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+import platform
+
+x = platform.system()
+print(x)
