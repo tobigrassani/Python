0f8a5b8c3deace7fdd246c9c29cdd9d5a63ac581
Tobias Grassani <tobigrassani@gmail.com>
2022-10-03 21:14:06 -0300

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,12 @@
+# Functions with iteration, loops, arrays, classes/objects, etcetera \n
+
+def myfunction():
+    print("HELLO WORLD, FROM myfunction, CHEERS0")
+
+
+def callfunctions():
+    x = input("Choose an option\n 1 - Call myfunction \n 2 - Exit \n")
+
+
+
+x = input("Choose an option\n 1 - Call myfunction \n 2 - Exit \n")
