0937901b4715d938bc7386a2f91c5b9078d35da0
Tobias Grassani <tobigrassani@gmail.com>
2022-10-10 01:27:18 -0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,12 @@
-youtuber = "el pendejito pendejin"
+youtuber = input("Youtuber: ")
+adj = input("Adjetive: ")
+age = input("Age: ")
+videos = input("Count: ")
+msg = input("Personal message: ")
 
-madlib = f"Suscribre to {youtuber}"
+madlib = f"Suscribe to {youtuber}, he always been too {adj}. actually, he is {age}. \
+{youtuber} already uploaded {videos} videos, and his message to the world is: '{msg}' "c
+
 
 print(madlib)
+
