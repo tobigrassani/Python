5b431834bf6d3fe3de6b9a638d13f399f57dc2c8
Tobias Grassani <tobigrassani@gmail.com>
2022-10-14 16:45:11 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -3,8 +3,8 @@ A = []
 
 for i in range(0, n):
     row = []
-    for j in range(0,n):
-        row.append(int(input(f"Insert the [{i}][{j}]: ")))
+    for j in range(0, n):
+        row.append(int(input(f"Insert [{i}][{j}] value: ")))
     A.append(row)
 
 print("Matrix A: \n", A, "\nSize: ", len(A))
