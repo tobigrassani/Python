30544f369f798e88d2e4413f0a92769fb7824634
Tobias Grassani <tobigrassani@gmail.com>
2022-10-14 16:31:43 -0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,9 @@
-size = int(input("Insert matrix size: "))
-A = []*size
-print("Matrix: \n", A, "\nSize: ", len(A))
+n = int(input("Insert list size: "))
+A = []
+
+for i in range(0, n):
+    x = int(input(f"Insert the {i+1}th element: "))
+    A.append(x)
+
+print("Matrix A: \n", A, "\nSize: ", len(A))
 
