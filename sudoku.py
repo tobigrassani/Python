f36d63745d087dc28e956aa2c5c8d5a87b896f33
Tobias Grassani <tobigrassani@gmail.com>
2022-10-13 22:53:29 -0300

    Update sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+import random
+
+size = 0
+while size <= 1:
+    size = input("Insert sudoku's matrix size: ")
+    if size <= 1:
+        print("Please insert a size bigger than 1.")
+
+
+
+
+
+
+
