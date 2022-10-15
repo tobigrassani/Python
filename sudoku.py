c5c6f85b60e76313840f7d9b13f58e8a72879846
Tobias Grassani <tobigrassani@gmail.com>
2022-10-15 16:01:16 -0300

    Update

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,17 @@
 n = int(input("Insert matrix size: "))
 A = []
 
-for i in range(0, n):
+for i in range(n):
     row = []
-    for j in range(0, n):
+    for j in range(n):
         row.append(int(input(f"Insert [{i}][{j}] value: ")))
     A.append(row)
 
 print("Matrix A: \n", A, "\nSize: ", len(A))
+print("Printing looped matrix: ")
+for i in A:
+    for j in i:
+        print(j, end=" ")
+    print()
+
 
