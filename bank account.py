a29afedd383099ef462a0daeee004b181d000de8
Tobias Grassani <tobigrassani@gmail.com>
2022-10-15 21:05:53 -0300

    Update bank account.py

--- bank account.py
+++ bank account.py
@@ -1,5 +1,18 @@
-def extract(t):
-    e = int(input("Insert amount to extract: "))
-    t -= e
-    print(f"Amount extracted: {e} \nNew total: {t}")
+def withdraw(t):
+    w = int(input("Insert amount to withdraw: "))
+    t -= w
+    print(f"Withdrawal amount: {w} \nNew Total: {t}")
+    return t
 
+
+def deposit(t):
+    d = int(input("Insert amount to deposit: "))
+    t += d
+    print(f"Deposited amount: {d} \nNew total: {t}")
+    return t
+
+
+total = int(input("Insert your account balance: "))
+total = deposit(total)
+total = withdraw(total)
+def main
