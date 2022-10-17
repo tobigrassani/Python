262c3c114817abb45b31cf83eed582b4370bccad
Tobias Grassani <tobigrassani@gmail.com>
2022-10-16 22:20:32 -0300

    Update bank account OOP.py

--- bank account OOP.py
+++ bank account OOP.py
@@ -1,9 +1,19 @@
 class Person:
-    def print(self):
-        print("Persons name is printed")
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+    def get_name(self):
+        return self.name
+
+    def get_gender(self):
+        return self.gender
 
 
-p = Person()
-p.name()
+p = Person("Alissa", "Female")
+print(f"Hello my name is {p.get_name().upper()}")
+print(f"and beeing {p.get_gender().lower()} is the best in world.")
+
+
 
 
