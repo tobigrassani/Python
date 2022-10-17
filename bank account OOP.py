6840ba27b98bc0428e3c18698efb042b2b7bc3ac
Tobias Grassani <tobigrassani@gmail.com>
2022-10-16 22:28:38 -0300

    Update bank account OOP.py

--- bank account OOP.py
+++ bank account OOP.py
@@ -14,6 +14,11 @@ p = Person("Alissa", "Female")
 print(f"Hello my name is {p.get_name().upper()}")
 print(f"and beeing {p.get_gender().lower()} is the best in world.")
 
+p2 = Person("Michael", "Male")
+print(f"Hello my name is {p2.get_name().upper()}")
+print(f"and beeing {p2.get_gender().lower()} is the best in world.")
+
+
 
 
 
