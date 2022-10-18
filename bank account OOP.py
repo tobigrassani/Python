4e5e3744c2ba699fc98b17b92dc48e22375a1086
Tobias Grassani <tobigrassani@gmail.com>
2022-10-18 12:29:09 -0300

    Update bank account OOP.py

--- bank account OOP.py
+++ bank account OOP.py
@@ -17,9 +17,9 @@ class Room:
     def add_person(self, person):
         if (len(self.people) < self.capacity) & (self.access == person.gender):     # filtering by capacity and gender
             self.people.append(person)
-            print(f"{person.name} added to {room1.name} successful")
+            print(f">>>>>{person.name} approved to join {room1.name}")
             return True
-        print(f"{person.name} was denied to add to {room1.name}")
+        print(f"{person.name} denied to join {room1.name}<<<<<")
         return False
 
 
@@ -27,12 +27,24 @@ p1 = Person("Alissa", "Female")
 p2 = Person("Michael", "Male")
 p3 = Person("Kelly", "Female")
 p4 = Person("Sally", "Female")
+p5 = Person("Maria", "Female")
+p6 = Person("Thomas", "Male")
+p7 = Person("John", "Male")
+p8 = Person("Alex", "Male")
+
+
+
 
 room1 = Room("Girl's Bathroom", "Female", 2)
+room2 = Room("Boy's Bathroom", "Male", 3)
 room1.add_person(p1)
 room1.add_person(p2)
 room1.add_person(p3)
 room1.add_person(p4)
+room2.add_person(p1)
+room2.add_person(p2)
+room2.add_person(p3)
+room2.add_person(p4)
 
 
 print(room1.people)
