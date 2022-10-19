e18c6f732c1593d8d43cd5bf29744e6f5e7ada2b
Tobias Grassani <tobigrassani@gmail.com>
2022-10-18 21:54:02 -0300

    Update bank account OOP.py

--- bank account OOP.py
+++ bank account OOP.py
@@ -14,7 +14,60 @@ class Person:
         return self.id
 
 
-class Account(Person):
+class Account:
+    def __init__(self):
+        self.balance = 0
+        print(">>> Hello!! Welcome to ATM <<<")
+
+    def deposit(self):
+        amount = int(input("\nEnter the amount to be deposited: "))
+        self.balance += amount
+        print(f"\nAmount deposited: ${amount} Usd")
+
+    def withdraw(self):
+        amount = -1
+        while amount <= 0:
+            amount = int(input("\nEnter the amount to be withdrawn: "))
+            if amount == 0:
+                print("Minimum to be withdrawn: $1 Usd")
+                continue
+            if self.balance < amount:
+                print("Insufficient balance")
+            else:
+                print(f"\nAmount withdrawn: ${amount} Usd")
+                self.balance -= amount
+
+    def display(self):
+        print(f"\nYour balance is ${self.balance} Usd")
+
+
+def main():
+    opt = True
+    while opt is True:
+        print("""\n
+        Menu Options:
+        1. Create Account
+        2. Deposit
+        3. Withdraw
+        0. Exit""")
+        opt = int(input("\nInsert option: "))
+        if opt == 1:
+            new_account = Account()
+            print("\nAccount Created Successfully")
+            opt = True
+        if opt == 2:
+            new_account.deposit()
+            opt = True
+        if opt == 3:
+            new_account.withdraw()
+            opt = True
+        if opt == 0:
+            print("\nThanks for using our machine.")
+            break
+
+
+main()
+
 
 
 
