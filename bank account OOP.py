885d1b81811e9cece2da30a5dbb4c12d56fe1e50
Tobias Grassani <tobigrassani@gmail.com>
2022-10-18 21:17:14 -0300

    Update bank account OOP.py

---
+++ bank account OOP.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self, name, gender, y_birth):
+        self.name = name
+        self.gender = gender
+        self.y_birth = y_birth
+
+    def get_gender(self):
+        return self.gender
+
+    def get_name(self):
+        return self.name
+
+    def get_y_birth(self):
+        return self.y_birth
+
+
+class Account:
+    
