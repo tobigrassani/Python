c2282ef00a4a9c5716dec8940cc02bda7f727c95
Tobias Grassani <tobigrassani@gmail.com>
2022-10-18 22:23:27 -0300

    Update

--- bank account OOP.py
+++ bank account OOP.py
@@ -15,30 +15,51 @@ class Person:
 
 
 class Account:
-    def __init__(self):
+    def __init__(self)
         self.balance = 0
+        self.accounts = []
         print(">>> Hello!! Welcome to ATM <<<")
+        self.id = int(input("Please insert your ID number: "))
 
     def deposit(self):
         amount = int(input("\nEnter the amount to be deposited: "))
         self.balance += amount
-        print(f"\nAmount deposited: ${amount} Usd")
+        print(f"\nAmount deposited: ${amount} USD")
 
     def withdraw(self):
         amount = -1
         while amount <= 0:
+            print(f"Available balance: ${self.balance} USD")
             amount = int(input("\nEnter the amount to be withdrawn: "))
             if amount == 0:
-                print("Minimum to be withdrawn: $1 Usd")
+                print("Minimum to be withdrawn: $1 USD")
                 continue
             if self.balance < amount:
                 print("Insufficient balance")
             else:
-                print(f"\nAmount withdrawn: ${amount} Usd")
+                print(f"\nAmount withdrawn: ${amount} USD")
                 self.balance -= amount
 
     def display(self):
         print(f"\nYour balance is ${self.balance} Usd")
+
+    def create_account(self):
+        if id in self.accounts:
+            print("Access Granted")
+        else:
+            print("""You don't have an account here.
+                  Do want to create one?
+                  Options:
+                  1. Yes
+                  2. No""")
+            opt = int(input("Insert option: "))
+            if opt == 1:
+                self.accounts.append(id)
+                print("\nAccount Created Successfully")
+            else:
+                pass
+
+
 
 
 def main():
