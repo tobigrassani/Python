7e957bf27877a7851ce49d7819941c8e2a34f469
Tobias Grassani <tobigrassani@gmail.com>
2022-10-20 23:12:27 -0300

    Update test - bathrooms OOP.py

--- test - bathrooms OOP.py
+++ test - bathrooms OOP.py
@@ -3,9 +3,6 @@ class Person:
         self.name = name
         self.gender = gender
 
-    def get_gender(self):
-        return self.gender
-
 
 class Room:
     def __init__(self, name, access, capacity):
@@ -17,35 +14,34 @@ class Room:
     def add_person(self, person):
         if (len(self.people) < self.capacity) & (self.access == person.gender):     # filtering by capacity and gender
             self.people.append(person)
-            print(f">>>>>{person.name} approved to join {self.name}")
             return True
-        print(f"{person.name} denied to join {self.name}<<<<<")
         return False
+
+    def del_person(self, person):
+        self.people.remove(person)
+
+    def show_people(self):
+        print(self.people)
 
 
 p1 = Person("Alissa", "Female")
 p2 = Person("Michael", "Male")
 p3 = Person("Kelly", "Female")
-p4 = Person("Sally", "Female")
-p5 = Person("Maria", "Female")
-p6 = Person("Thomas", "Male")
-p7 = Person("John", "Male")
-p8 = Person("Alex", "Male")
-
 
 room1 = Room("Girl's Bathroom", "Female", 2)
 room2 = Room("Boy's Bathroom", "Male", 3)
-print(
-    room1.add_person(p1),
-    room1.add_person(p2),
-    room1.add_person(p3),
-    room1.add_person(p4))
 
+room1.add_person(p1)
+room1.add_person(p2)
+room1.add_person(p3)
 
 room2.add_person(p1)
 room2.add_person(p2)
 room2.add_person(p3)
-room2.add_person(p4)
+
+room1.show_people()
+room2.show_people()
+
 
 
 
