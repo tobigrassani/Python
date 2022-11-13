fc178b66b7c482cbfee69cb872e6f5612dc53dd9
Tobias Grassani <tobigrassani@gmail.com>
2022-11-13 16:41:35 -0300

    Update Ford Fulkerson.py

--- Ford Fulkerson.py
+++ Ford Fulkerson.py
@@ -23,17 +23,9 @@ class Graph:
         # Algoritmo BPA
         while queue:
 
-            # Dequeue a vertex from queue and print it
             u = queue.pop(0)
-
-            # Get all adjacent vertices of the dequeued vertex u
-            # If a adjacent has not been visited, then mark it
-            # visited and enqueue it
             for index, value in enumerate(self.graph[u]):
                 if visited[index] == False and value > 0:
-                    # If we find a connection to the sink node,
-                    # then there is no point in BFS anymore
-                    # We just have to set its parent and can return true
                     queue.append(index)
                     visited[index] = True
                     parent[index] = u
@@ -43,31 +35,25 @@ class Graph:
         # No existe camino entre la fuente y el sumidero, entonces return False
         return False
 
-    # Returns the maximum flow from s to t in the given graph
+    # Metodo FordFulkerson para devolver el flujo maximo entre fuente y sumidero
     def FordFulkerson(self, source, sink):
 
-        # This array is filled by BFS and to store path
+        # Arreglo rellenado por el BPA
         parent = [-1] * self.ROW
 
-        max_flow = 0  # There is no flow initially
+        max_flow = 0  # Flujo inicial es 0
 
-        # Augment the flow while there is path from source to sink
+        # Aumenta el flujo mientras va encontrando camino
         while self.BFS(source, sink, parent):
 
-            # Find minimum residual capacity of the edges along the
-            # path filled by BFS. Or we can say find the maximum flow
-            # through the path found.
             path_flow = float("Inf")
             s = sink
             while (s != source):
                 path_flow = min(path_flow, self.graph[parent[s]][s])
                 s = parent[s]
 
-            # Add path flow to overall flow
             max_flow += path_flow
 
-            # update residual capacities of the edges and reverse edges
-            # along the path
             v = sink
             while (v != source):
                 u = parent[v]
