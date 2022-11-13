23181d412d65d9eb4692cf1cc29dac44ed33a918
Tobias Grassani <tobigrassani@gmail.com>
2022-11-13 13:46:00 -0300

    Update Recorrido euleriano.py

--- Recorrido euleriano.py
+++ Recorrido euleriano.py
@@ -1,117 +1,129 @@
-import random
+def camino_euleriano():
+    print("Introduzca cada entrada separada por coma (,) \n")
+    print("Ejemplo: \nConjunto de vértices: a,b,c \nBordes: (a,b),(a,c),(b,a),(b,c) \n")
 
-Grafo = dict()
-Grafo_variable = dict()
-nodos = []
-Conversion_nodos = []
+    sov = input("Conjunto de vértices: ")  # Obtener letras de vértice de entrada
+    Ssov = sov.split(",")  # Obtener los vértices de entrada delimitados por comas y ponerlos en el array
 
-print("Cantidad de NODOS: ")
-num_de_nodos = int(input())
+    edg = input("Bordes: ")
+    edg1 = edg.replace  (",", "")  # Borrar todas las comas (,) de la entrada Bordes
+    edg2 = edg1.replace("(", "")  # Borrar todos los parentesis de apertura de la entrada Bordes
+    edg3 = edg2.replace(")", "")  # Borrar todos los parentesis de cierre de la entrada Bordes
 
-print('Ingrese la lista de adyacencia para cada nodo y separe los elementos con comas (,)')
+    outdeg = []  # Creación de lista de fuera de grado vacía
+    indeg = []  # Creación de una lista en grados vacía
 
-for i in range(num_de_nodos):
-    print("nodo", i + 1)
-    nodos = str(input())
-    Conversion_nodos = list(nodos)
-    Grafo[i + 1] = Conversion_nodos
+    odCounter = 0  # Declarar contador de-grado de salida igual a 0
+    idCounter = 0  # Declarar contador fuera-de-grado de salida igual a 0
 
-print("Fin de input lista adyacencia. ")
+    equalDegrees = 0  # Variable para acumular cuando los grados en-grado y fuera-de-grado son iguales
+    notEqualDegrees = 0
 
-Grafo_variable = Grafo
-num_aristas = 0
+    EPEequalDegrees = 0  # Variable para acumular todos los vértices pares
+    EPOequalDegrees = 0
 
-num_elementos_list = len(Grafo)
-nodo_inicio = random.randint(1, num_elementos_list)
-Solucion = []
+    ECreason = 0  # Variable por la razón de no tener un motivo del circuito Euleriano
+    EPreason = 0  # Variable por la razón de no tener una razón Camini Euleriano
 
-print('El nodo de inicio del circuito es', nodo_inicio)
+    eulerPath = False
+    ep1 = 0
+    ep2 = 0
 
-valencia_nodos = 0
-ultimo_nodo_borrado = 0
+    eulerCircuit = False
+    ec = 0
 
-Solucion.append(nodo_inicio)
-for i in Grafo:
-    num_aristas += len(Grafo[i])
+    forCounter = 0
 
-num_aristas = num_aristas / 2
-num_aristas_var = num_aristas
+    for j in Ssov:  ## Iterar a través de cada vértice
 
-while (num_aristas_var != 0):
-    nodo = nodo_inicio
-    valencia_nodos = 0
-    check_nodo = 0
+        ## Ver si el vértice es igual a outdegree
 
-for i in Grafo_variable[nodo]:
-    for j in Grafo_variable[nodo]:
-        if j != 0:
-            valencia_nodos += 1
+        ## Fuera-de-grado
+        for i in range(0, len(edg3), 2):
+            op, code = edg3[
+                       i:i + 2]  ## Obtener Bordes 2 por 2 de la lista Bordes y asignar la primera letra a op y la segunda a code
+            ## op comparará el grado de salida. El código se comparará en grado
 
-    if valencia_nodos == 0 & num_aristas_var <= 1:
-        for i in Grafo_variable[Solucion[-1]]:
-            if i == 0:
-                Grafo_variable[Solucion[-1]][Grafo_variable[Solucion[-1]].index(i)] = Solucion[-2]
-                break
-        for i in Grafo_variable[Solucion[-2]]:
-            if i == 0:
-                Grafo_variable[Solucion[-2]][Grafo_variable[Solucion[-2]].index(i)] = Solucion[-1]
-                break
+            if j == op:  ## Si el vértice actual es igual a los primeros anuncios de ocurrencia de outdegree en el contador
+                odCounter = odCounter + 1  ## Añadir un outdegree a un vértice
 
-        ultimo_nodo_borrado = Solucion.pop()
-        num_aristas_var += 1
-        valencia_nodos = 0
+        outdeg.append(odCounter)  ## Añada la cantidad de ocurrencias de un vértice en la lista de salida
+        odCounter = 0;  ## Establecer odCounter igual a 0
 
-        for j in Grafo_variable[Solucion[-1]]:
-            if j != 0:
-                valencia_nodos += 1
+        ## En-grado
+        ## Repetir todos los pasos desde En-grado hasta Fuera-de-grado
+        for i in range(0, len(edg3), 2):
+            op, code = edg3[i:i + 2]
 
-        if valencia_nodos == 1:
-            while valencia_nodos == 1:
-                for i in Grafo_variable[Solucion[-1]]:
-                    if i == 0:
-                        Grafo_variable[Solucion[-1]][Grafo_variable[Solucion[-1]].index(i)] = Solucion[-2]
-                        break
-                for i in Grafo_variable[Solucion[-2]]:
-                    if i == 0:
-                        Grafo_variable[Solucion[-2]][Grafo_variable[Solucion[-2]].index(i)] = Solucion[-1]
-                        break
-                ultimo_nodo_borrado = Solucion.pop()
+            if j == code:
+                odCounter = odCounter + 1
 
-                num_aristas_var += 1
-                valencia_nodos = 0
+        indeg.append(odCounter)
+        odCounter = 0;
 
-                for j in Grafo_variable[Solucion[-1]]:
-                    if j != 0:
-                        valencia_nodos += 1
+    for j, o, i in zip(Ssov, outdeg, indeg):
+        print(str('deg-('), j, str(')= '), o, str(', deg+('), j, str(')= '), i,
+              str('\n'))  # Printing the values for every vertex letter and corresponding in and out degrees
 
-            nodo_inicio = Solucion[-1]
-            break
+        ## Chequear por circuito Euleriano
 
+        if (o == i):
+            equalDegrees = equalDegrees + 1  ## Cada vez que un En-grado y Fuera-de-grado sean iguales, anadir al contador
         else:
-            nodo_inicio = Solucion[-1]
-            break
+            notEqualDegrees = notEqualDegrees + 1  ## Cada vez que un vertice Fuera-de-grado y En-grado no sean iguales, anadir al contador
 
+        if (notEqualDegrees == 0):  ## Si los vertices nunca fueron no iguales, entoces tenemos un circuito Euleriano
+            eulerCircuit = True
+        else:
+            eulerCircuit = False
+            ECreason = 2
 
+        ## Chequear por un camino Euleriano
+        if ((o - i) == 1):
+            ep1 = ep1 + 1
+
+        if ((i - o) == 1):
+            ep2 = ep2 + 1
+
+        ##  Verificar que uno si y uno no de cada vertex tenga En-grado =Fuera-de-grado
+        if ((forCounter % 2) == 0):
+            if (o == i):
+                EPEequalDegrees = EPEequalDegrees + 1
+        else:
+            if (o == i):
+                EPOequalDegrees = EPOequalDegrees + 1
+        forCounter = forCounter + 1
+
+    ## End for
+
+    if (EPEequalDegrees >= 1 or EPOequalDegrees >= 1):
+        if (ep1 == 1 and ep2 == 1):
+            eulerPath = True
+        else:
+            eulerPath = False
+            EPreason = 1
     else:
-        if i != 0 and i != ultimo_nodo_borrado:
-            ultimo_nodo_borrado = 0
+        EPreason = 2
 
-            for k in Grafo_variable[nodo]:
-                if k == i:
-                    Grafo_variable[nodo][Grafo_variable[nodo].index(k)] = 0
+    print(str('Circuito Euleriano: '), eulerCircuit)
+    print(str('Camino Euleriano: '), eulerPath)
 
-            for l in Grafo_variable[i]:
-                if l == nodo:
-                    Grafo_variable[i][Grafo_variable[i].index(l)] = 0
+    if (eulerCircuit == True):
+        print("La grafica no tiene un Circuito Euleriano")
+    else:
+        if (ECreason == 2):
+            print(
+                "La grafica no tiene un circuito euleriano porque el En-grado y Fuera-de-grado de cada vertice no son iguales")
 
-            num_aristas_var -= 1
-            nodo_inicio = i
-            valencia_nodos = 0
-            Solucion.append(i)
-            break
+    if (eulerPath == True):
+        print("La grafica tiene un camino Euleriano")
+    else:
+        if (EPreason == 1):
+            print(
+                "La grafica no tiene un camino Euleriano porque ni a lo más un vértice tiene (fuera-de-grado) - (en-grado) = 1, tampoco tiene como mucho un vértice que tenga (en-grado) - (fuera-de-grado) = 1")
+        elif (EPreason == 2):
+            print("La grafica no tiene un camino Euleriano porque cada otro vértice no tiene en-grado = fuera-de-grado")
 
-print
-print('El circuito de EULER es:', Solucion)
-print
-print(Solucion)
-print
+
+EulerianoCNN()
+
